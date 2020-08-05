d60b46e3ef8171df2e593649504217210bce144d
Nathaniel Glen <njag2202@gmail.com>
2020-08-04 23:40:40 -0400

    pipewire: 0.3.7 -> 0.3.9

--- pkgs/development/libraries/pipewire/default.nix
+++ pkgs/development/libraries/pipewire/default.nix
@@ -33,7 +33,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "pipewire";
-  version = "0.3.7";
+  version = "0.3.9";
 
   outputs = [ "out" "lib" "dev" "doc" ];
 
@@ -42,8 +42,10 @@ stdenv.mkDerivation rec {
     owner = "pipewire";
     repo = "pipewire";
     rev = version;
-    sha256 = "04l66p0wj553gp2zf3vwwh6jbr1vkf6wrq4za9zlm9dn144am4j2";
+    sha256 = "0q781r32mnm3qy6xcdd2rnb8g50gdi7mi50zmdiq24s24sr8f8r9";
   };
+
+  patches = [ ./alsa-profiles-use-libdir.patch ];
 
   nativeBuildInputs = [
     doxygen
@@ -78,6 +80,7 @@ stdenv.mkDerivation rec {
     "-Ddocs=true"
     "-Dman=false" # we don't have xmltoman
     "-Dgstreamer=true"
+    "-Dudevrulesdir=lib/udev/rules.d"
   ];
 
   FONTCONFIG_FILE = fontsConf; # Fontconfig error: Cannot load default config file
